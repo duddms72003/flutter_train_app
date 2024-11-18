80b27d7469824a2edafc167505884af288c1b9f6
duddms72003 <duddms72003@naver.com>
2024-11-18 23:40:31 +0900

    add seat_page

--- lib/pages/widgets/seat_page.dart
+++ lib/pages/widgets/seat_page.dart
@@ -7,6 +7,66 @@ class SeatPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('좌석 선택'),
       ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 60),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '출발역',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                    color: Colors.purple,
+                  ),
+                ),
+                Spacer(),
+                Icon(Icons.arrow_circle_right_outlined),
+                Spacer(),
+                Text(
+                  '도착역',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                    color: Colors.purple,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 20,
+                  height: 20,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.purple,
+                  ),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text('선택됨'),
+                Container(
+                  width: 20,
+                  height: 20,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text('선택됨'),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
